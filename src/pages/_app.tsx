e832aa0b70d0b3139c6d5447f6bb923e5ca6b492
Mateus <mateuscg42@gmail.com>
2022-02-06 22:28:50 -0300

    API Routes NextJs & FaunaDb Config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import { AppProps } from 'next/app';
+import { SessionProvider as NextAuthProvider } from 'next-auth/react';
+
+import '../../styles/global.scss';
+import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <NextAuthProvider session={pageProps.session}>
+      <Header />
+      <Component {...pageProps} />
+    </NextAuthProvider>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,87 @@
-import Head from 'next/head';
-import Image from 'next/image';
+// import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
-export default function Home() {
-  return <h1>Hello World</h1>;
+import Head from 'next/head';
+
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+
+import styles from './home.module.scss';
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
 }
+
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
+}
+
+// SERVER SIDE RENDERING (SSR)
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const price = await stripe.prices.retrieve('price_1KOtRCBiYNZ5C1qIUlRcKTdp', {
+//     expand: ['product'],
+//   });
+
+//   // mostrar nome do produto
+//   // const price = await stripe.prices.retrieve('price_1KOtRCBiYNZ5C1qIUlRcKTdp', {
+//   //   expand: ['product'],
+//   // });
+
+//   const product = {
+//     priceId: price.id,
+//     amount: new Intl.NumberFormat('en-US', {
+//       style: 'currency',
+//       currency: 'USD',
+//     }).format(price.unit_amount / 100),
+//   };
+
+//   return {
+//     props: {
+//       product,
+//     },
+//   };
+// };
+
+// STATIC SITE GENERATION (SSG)
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KOtRCBiYNZ5C1qIUlRcKTdp');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24,
+  };
+};
